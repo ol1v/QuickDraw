97f1ac00b113cdbaed3d40085fadbb727c0b6e66
ol1v <oliverhenriklennartsson@gmail.com>
2024-06-19 17:25:50 +0200

    updated pre cfg to load on install

--- background.js
+++ background.js
@@ -1,3 +1,37 @@
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === 'install') {
+      console.log('Extension installed for the first time.');
+      fetch(chrome.runtime.getURL('data/predefined_sites.json'))
+        .then(response => response.json())
+        .then(predefinedSites => {
+          // Get current sites from storage
+          chrome.storage.sync.get('sites', function(data) {
+            const sites = data.sites || [];
+            
+            // Add predefined sites to the current sites
+            predefinedSites.forEach(site => {
+              sites.push({
+                name: site.name,
+                urls: {
+                  ip: site.urls.ip,
+                  domain: site.urls.domain,
+                  hash: site.urls.hash
+                }
+              });
+            });
+  
+            // Save updated sites to storage
+            chrome.storage.sync.set({ sites }, function() {
+              console.log('Predefined sites added.');
+            });
+          });
+        })
+        .catch(error => {
+          console.error('Error loading predefined sites:', error);
+        });
+    }
+  });  
+  
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
     if (message.action === "createNewTab") {
         const newTabUrl = message.text;
